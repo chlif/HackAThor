803df008da779680644c2ca3faf33075c313a986
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2015-01-10 12:42:20 +0200

    Try to create bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	/*
+	 * Only single 'bullet' can be moving at one time
+	 * Shotguns shoot 'bullet's
+	 */
+
+	// whether bullet is moving atm
+	bool isMoving = false;
+	float bulletSpeed = 8f;
+	float timeUntilReset = 1.5f;
+	private float timeElapsed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	//Check for time validity
+		if(timeElapsed > timeUntilReset){
+			// TODO transform back to outside of the screen
+			// TODO set velocity to zero
+		}else{
+			timeElapsed += Time.deltaTime;
+		}
+	//Update position
+		this.gameObject.transform.position += bulletSpeed * this.gameObject.transform.forward;
+
+
+
+	}
+
+	//Check for collision events
+	void onCollisionEnter(Collision collision){
+		if(collision.gameObject.name == "Zombie"){ // gameObject type!
+			Destroy(collision.gameObject);
+			//TODO transform this.gameObject back to outside of the screen
+		}
+	}
+}
--- Assets/Scripts/Shotgun.cs
+++ Assets/Scripts/Shotgun.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Shotgun : MonoBehaviour {
 
+	public Bullet bullet;
+
 	// Use this for initialization
 	void Start () {
 
@@ -10,6 +12,25 @@ public class Shotgun : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown( KeyCode.G )) { 
+			fireShotgun();
+		}
+	}
 
+	void fireShotgun(){
+		CreateNewBullet ();
+		//bullet.transform.position( this.transform.localPosition.x, this.transform.localPosition.y, this.transform.localPosition.z );
+	}
+
+	void CreateNewBullet() {
+		Rigidbody clone;
+		float speed = 5.0f;
+		GameObject ob;
+		ob = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+		clone = (Rigidbody) ob.GetComponent(typeof(Rigidbody));
+		clone.velocity = transform.TransformDirection ( new Vector3 (4, 0, 0));
+//		clone.AddForce (clone.transform.forward * speed);
+
+		//Destroy (clone.gameObject, 3);
 	}
 }
