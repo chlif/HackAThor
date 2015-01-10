46a91a86b7f5fbfbe84cb95874f66c2bff5e84f5
Tuomas Paasonen <tuomas.paasonen@futurice.com>
2015-01-10 09:53:03 +0200

    Add hero animations

---
+++ Assets/Scripts/HeroTorso.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroTorso : MonoBehaviour {
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 parentVelocity = transform.parent.rigidbody2D.velocity;
+		float xVelocity = parentVelocity.x;
+
+		anim.SetFloat ("speed", xVelocity);
+	}
+}
